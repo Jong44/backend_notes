964da65efa3c591262e7bc422eaebb995ff1e7cd
Jong44 <mat.skak45@gmail.com>
2024-10-09 23:29:16 +0700

    update

--- app/app.js
+++ app/app.js
@@ -5,9 +5,6 @@ const app = express();
 const NoteRoutes = require('./routes/NoteRoutes');
 const AuthRoutes = require('./routes/AuthRoutes');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
 app.use('/notes', NoteRoutes);
 app.use('/auth', AuthRoutes);
 
